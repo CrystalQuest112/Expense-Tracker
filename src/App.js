20b2a5e7393ac8b19cc7765250a73b91097b9661
CrystalQuest112 <114012059+CrystalQuest112@users.noreply.github.com>
2023-01-09 16:58:41 +0200

    + Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Exitems from "./components/exitems";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Groceries",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Fitness Equipment",
+      amount: 389.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 194.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "Furniture",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Expense Tracker</h1>
+      <Exitems
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <Exitems
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <Exitems
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
     </div>
   );
 }
---
+++ src/components/exitems.js
@@ -0,0 +1,16 @@
+import ExDate from "./ExDate";
+import "./exitems.css";
+
+function ExItems(props) {
+  return (
+    <div className="expense-item">
+      <ExDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount} </div>
+      </div>
+    </div>
+  );
+}
+
+export default ExItems;
