7a769f4f939bdef7fb1dcaabf2191e243d12a91b
CrystalQuest112 <114012059+CrystalQuest112@users.noreply.github.com>
2023-01-23 18:22:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 import "./App.css";
 import ExpDisplay from "./components/ExpDisplay";
 import NewExp from "./components/NewExpense/newExp";
+import React, { useState } from "react";
+
+const DM_EXPENSES = [
+  {
+    id: "e1",
+    title: "Groceries",
+    amount: 407.99,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "Fitness Equipment",
+    amount: 389.49,
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 194.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "Furniture",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 function App() {
   const [expenses, setExpenses] = useState(DM_EXPENSES);
